e74c47fea7f79b9dd45c16f1a65b7c37c7cd5d4a
tangui.aladjidi <tangui.aladjidi@polytechnique.edu>
2020-04-09 14:19:45 +0200

    Defined a main function for the program to run more smoothly in command line and python.

--- examples/harambe/harambe.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python
-# -*- coding: utf-8 -*-
-import compute_cgh
-
-compute_cgh("-h")
-compute_cgh("harambe_512.bmp", "I0.bmp", "cgh_conf.conf", phi0="calib_512.bmp", output ="results") 
-
-
-
